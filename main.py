63d188debc7407b094ce81f08edf5aa7930ef5d3
J0w03L <git@j0w03l.me>
2024-04-26 12:00:12 +0100

    Improve debug logging; error handling if rc_resources.py is missing

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import logging
 # Setup logging level and format.
 logging.basicConfig(
     level   = logging.DEBUG,
-    format  = "%(asctime)s - [%(levelname)s] %(name)s: %(message)s"
+    format  = "%(asctime)s - [%(levelname)s] %(name)s::%(lineno)d (%(funcName)s): %(message)s"
 )
 logger = logging.getLogger(__name__)
 
@@ -13,6 +13,7 @@ try:
     from PySide6.QtWidgets import QApplication
 except ModuleNotFoundError:
     # The user does not have PySide6 installed; let them know that they need to install it.
+    # We don't use logger for this; logger is primarily for debugging purposes.
     sys.stderr.write(
         "\n".join(
             [
@@ -23,13 +24,29 @@ except ModuleNotFoundError:
             ]
         ) + "\n"
     )
+
+    # Exit with a non-zero status code to indicate an error occurred.
+    sys.exit(1)
+
+# Try to import resources such as images and audio.
+# We don't need to use them in here, but if they don't exist, we can catch it early here.
+try:
+    import rc_resources
+except ModuleNotFoundError:
+    sys.stderr.write(
+        "\n".join(
+            [
+                "py-QtRPSLS could not find rc_resources.py; this application cannot run!",
+                "",
+                "Please re-download py-QtRPSLS or run the following command in this program's root directory:",
+                "    pyside6-rcc resources.qrc -o rc_resources.py"
+            ]
+        ) + "\n"
+    )
     sys.exit(1)
 
 from src.utils.Database import GameDB
 from src.MainMenu import MainMenu
-
-# Import resources such as images and audio.
-import rc_resources
 
 if __name__ == "__main__":
     logger.debug("Loading database...")
